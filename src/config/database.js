66c6d5442c53685406a3d639e69e72d4483c69a9
Angel Mena <angelmeba4@gmail.com>
2024-10-14 08:33:45 -0600

    Cambio host

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,11 @@
 import { Sequelize } from 'sequelize';
 
 // Create a Sequelize instance
-const sequelize = new Sequelize('dboNPANTS7', 'root', '12345678', {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    logging: false // Disable logging for a cleaner output
+const sequelize = new Sequelize('railway', 'root', 'wlHseHzAADtPlnKBWecHIWoysqsJoVJK', { 
+       host: 'autorack.proxy.rlwy.net',
+        dialect: 'mysql',
+    logging: false,
+    port: 21008 // Disable logging for a cleaner output
 });
 
 // Test the connection inside an async function
